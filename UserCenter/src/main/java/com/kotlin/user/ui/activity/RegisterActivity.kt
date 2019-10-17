8a6ac33b3d6b3d6afe8e28073d10d72113c69c5b
wz <291171110@qq.com>
2019-10-17 19:36:30 +0800

    初始化用户模块；动态加载模块。

---
+++ UserCenter/src/main/java/com/kotlin/user/ui/activity/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.kotlin.user.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kotlin.user.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
