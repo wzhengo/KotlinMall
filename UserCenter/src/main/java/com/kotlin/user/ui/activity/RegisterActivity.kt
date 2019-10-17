beaf9c270751828ab3d4d402445ab35140a45e12
wz <291171110@qq.com>
2019-10-17 20:55:58 +0800

    引入anko

--- UserCenter/src/main/java/com/kotlin/user/ui/activity/RegisterActivity.kt
+++ UserCenter/src/main/java/com/kotlin/user/ui/activity/RegisterActivity.kt
@@ -3,11 +3,18 @@ package com.kotlin.user.ui.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.kotlin.user.R
+import kotlinx.android.synthetic.main.activity_register.*
+import org.jetbrains.anko.toast
+
 
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        mRegisterBtn.setOnClickListener {
+            toast("注册")
+        }
+
     }
 }
