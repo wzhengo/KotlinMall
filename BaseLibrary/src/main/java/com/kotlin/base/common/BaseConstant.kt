446d72559e3baeb1d5bd3f7cf5c9595ec0404a19
wz <291171110@qq.com>
2019-10-18 21:07:58 +0800

    Retrofit集成与单例工厂

---
+++ BaseLibrary/src/main/java/com/kotlin/base/common/BaseConstant.kt
@@ -0,0 +1,12 @@
+package com.kotlin.base.common
+
+/**
+ * @author wangzhen
+ * @date 2019/10/18
+ */
+class BaseConstant {
+
+    companion object {
+        const val SERVER_ADDRESS = ""
+    }
+}
