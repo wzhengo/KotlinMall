c71fe1e43baa5fef5614b91376e985809e4fff0a
wz <291171110@qq.com>
2019-10-22 20:55:40 +0800

    HeaderBar封装

---
+++ BaseLibrary/src/main/java/com/kotlin/base/widgets/HeaderBar.kt
@@ -0,0 +1,56 @@
+package com.kotlin.base.widgets
+
+import android.app.Activity
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.FrameLayout
+import com.kotlin.base.R
+import com.kotlin.base.ext.onClick
+import kotlinx.android.synthetic.main.layout_header_bar.view.*
+
+/**
+ * @author wangzhen
+ * @date 2019/10/22
+ */
+class HeaderBar @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+
+    private var isShowBack = true
+    private var titleText: String? = null
+    private var rightText: String? = null
+
+    init {
+        val typeArray = context.obtainStyledAttributes(attrs, R.styleable.HeaderBar)
+        isShowBack = typeArray.getBoolean(R.styleable.HeaderBar_isShowBack, true)
+        titleText = typeArray.getString(R.styleable.HeaderBar_titleText)
+        rightText = typeArray.getString(R.styleable.HeaderBar_rightText)
+
+        initView()
+        typeArray.recycle()
+    }
+
+    private fun initView() {
+        View.inflate(context, R.layout.layout_header_bar, this)
+
+        mLeftIv.visibility = if (isShowBack) View.VISIBLE else View.GONE
+
+        //标题不为空，设置值
+        titleText?.let {
+            mTitleTv.text = it
+        }
+
+        //右侧文字不为空，设置值
+        rightText?.let {
+            mRightTv.text = it
+            mRightTv.visibility = View.VISIBLE
+        }
+
+        mLeftIv.onClick {
+            if (context is Activity) {
+                (context as Activity).finish()
+            }
+        }
+    }
+}
