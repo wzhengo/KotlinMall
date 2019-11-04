f1a54d7e0979c0ead0e888b3303c705db5be92e9
wz <291171110@qq.com>
2019-11-05 00:12:19 +0800

    底部导航开发

--- App/src/main/java/com/kotlin/mall/activity/MainActivity.kt
+++ App/src/main/java/com/kotlin/mall/activity/MainActivity.kt
@@ -1,12 +1,23 @@
-package com.wz.kotlinmall
+package com.kotlin.mall.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.wz.kotlinmall.R
+import kotlinx.android.synthetic.main.activity_main.*
+import rx.Observable
+import rx.android.schedulers.AndroidSchedulers
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mBottomNavBar.checkCartBadge(20)
+        mBottomNavBar.checkMsgBadge(false)
+
+        Observable.timer(2,TimeUnit.SECONDS)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { mBottomNavBar.checkMsgBadge(true) }
     }
 }
